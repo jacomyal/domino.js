80fc7c2239e3dabad47fea0fd6e387fe62e4b97e
jacomyal <alexis.jacomy@gmail.com>
2012-10-09 14:03:28 +0200

    added unit tests for complex properties

--- test/unit.properties.js
+++ test/unit.properties.js
@@ -51,19 +51,40 @@ test('test 1', function() {
 
   // Here we simply update the value:
   o.m.s.dispatch('def');
-  deepEqual(o.m.s.historic[o.m.s.historic.length-1], 'def', 'The property has been successfully updated!');
+  deepEqual(o.m.s.historic[o.m.s.historic.length-1], 'def', 'The string property has been successfully updated!');
 });
 test('test 2', function() {
   var o = init([{ id: 's', type: 'string', value: 'abc', triggers: 'updateS', dispatch: 'sUpdated'}]);
 
   // Here we update the property, but with a different value, and we want to check that domino has triggered:
   o.m.s.dispatch('def');
-  deepEqual(o.m.s.historic.length, 2, 'Updating the property with a new value effectively triggers the events.');
+  deepEqual(o.m.s.historic.length, 2, 'Updating the string property with a new value effectively triggers the events.');
 });
 test('test 3', function() {
   var o = init([{ id: 's', type: 'string', value: 'abc', triggers: 'updateS', dispatch: 'sUpdated'}]);
 
   // Here we update the property, but with the same value, and we want to check that domino does not trigger:
   o.m.s.dispatch('abc');
-  deepEqual(o.m.s.historic.length, 1, 'Updating the property with the same value does not trigger the events.');
+  deepEqual(o.m.s.historic.length, 1, 'Updating the string property with the same value does not trigger the events.');
+});
+test('test 4', function() {
+  var o = init([{ id: 's', type: {a: 'number', b: '?number'}, value: {a: 1}, triggers: 'updateS', dispatch: 'sUpdated'}]);
+
+  // Here we simply update the value:
+  o.m.s.dispatch({a: 2, b: 1});
+  deepEqual(o.m.s.historic[o.m.s.historic.length-1], {a: 2, b: 1}, 'The complex property has been successfully updated!');
+});
+test('test 5', function() {
+  var o = init([{ id: 's', type: {a: 'number', b: '?number'}, value: {a: 1}, triggers: 'updateS', dispatch: 'sUpdated'}]);
+
+  // Here we update the property, but with a different value, and we want to check that domino has triggered:
+  o.m.s.dispatch({a: 2, b: 1});
+  deepEqual(o.m.s.historic.length, 2, 'Updating the complex property with a new value effectively triggers the events.');
+});
+test('test 6', function() {
+  var o = init([{ id: 's', type: {a: 'number', b: '?number'}, value: {a: 1}, triggers: 'updateS', dispatch: 'sUpdated'}]);
+
+  // Here we update the property, but with the same value, and we want to check that domino does not trigger:
+  o.m.s.dispatch({a: 1});
+  deepEqual(o.m.s.historic.length, 1, 'Updating the complex property with the same value does not trigger the events.');
 });
