2e56529048f66287847ad3497f917ce4e5de09af
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-08-07 15:20:07 +0200

    Added events triggerer
    It works with the "on", "off" and "trigger" methods, but the code is exactly the same as in the original domino.
    Only modifications: No more getEvent method, no more split on spaces. Any string can now be an event name.
    Also ported the related unit tests that are still relevant.

---
+++ test/test.events.js
@@ -0,0 +1,74 @@
+module('domino.triggerer');
+
+test('Basics', function() {
+  // 1. Basics
+  var count = 0,
+      instance = new domino.triggerer(),
+      callback = function() { count++; };
+
+  instance.trigger('myEvent');
+  strictEqual(count, 0, 'Dispatching an event with no callback does nothing.');
+
+  instance.on('myEvent', callback);
+  instance.trigger('myEvent');
+  strictEqual(count, 1, 'Dispatching an event with a callback executes the callback.');
+
+  instance.off('myEvent', callback);
+  instance.trigger('myEvent');
+  strictEqual(count, 1, 'Dispatching an event with a callback than has been unbound does nothing.');
+});
+
+test('API', function() {
+  // 1. "unbind" polymorphism
+  var count = 0,
+      instance = new domino.triggerer(),
+      callback = function() { count++; };
+
+  instance.on('myEvent', callback);
+  instance.off('myEvent', callback);
+  instance.trigger('myEvent');
+  strictEqual(count, 0, 'unbind(event, handler) works.');
+
+  instance.on('myEvent', callback);
+  instance.off(['myEvent', 'anotherEvent'], callback);
+  instance.trigger('myEvent');
+  strictEqual(count, 0, 'unbind(event, handler) works.');
+
+  instance.on('myEvent', callback);
+  instance.off('myEvent');
+  instance.trigger('myEvent');
+  strictEqual(count, 0, 'unbind(event) works.');
+
+  instance.on('myEvent', callback);
+  instance.off();
+  instance.trigger('myEvent');
+  strictEqual(count, 0, 'unbind() works.');
+
+  // 2. "bind" polymorphism
+  var count1 = 0,
+      count2 = 0,
+      instance = new domino.triggerer(),
+      callback1 = function() { count1++; },
+      callback2 = function() { count2++; };
+
+  instance.on('myEvent1', callback1);
+  instance.trigger('myEvent1');
+  strictEqual(count1, 1, 'bind(event, handler) works.');
+  instance.off('myEvent1');
+  count1 = 0;
+
+  instance.on(['myEvent1', 'myEvent2'], callback1);
+  instance.trigger('myEvent1');
+  instance.trigger('myEvent2');
+  strictEqual(count1, 2, 'bind(["event1", "event2"], handler) works.');
+  instance.off(['myEvent1', 'myEvent2']);
+  count1 = 0;
+console.log('BIIIIIIM');
+  instance.on({ myEvent1: callback1, myEvent2: callback2 });
+  instance.trigger('myEvent1');
+  instance.trigger('myEvent2');
+  deepEqual([count1, count2], [1, 1], 'bind({ event1: callback1, event2: callback2, }, handler) works.');
+  instance.off(['myEvent1', 'myEvent2']);
+  count1 = 0;
+  count2 = 0;
+});
