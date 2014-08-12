6b45026c76a4e512366b5ee0795f40cffdb0277c
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-08-12 17:22:43 +0200

    Fixed grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,6 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
   grunt.registerTask('default', ['closureLint', 'jshint', 'browserify:build', 'qunit']);
-  grunt.registerTask('watch', ['browserify:watch']);
+  grunt.registerTask('watch', ['browserify:build', 'browserify:watch']);
   grunt.registerTask('test', ['qunit']);
 };
