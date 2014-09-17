6a419c5adcfd04272116d8e33260257affdc6f2d
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-09-17 14:10:22 +0200

    Added facets test into collection

--- test/unit.collection.js
+++ test/unit.collection.js
@@ -1,5 +1,6 @@
 // TODO: add relevant tests when code is done.
 module.exports = {
   helpers: require('./suites/helpers.test.js'),
-  emitter: require('./suites/emitter.test.js')
+  emitter: require('./suites/emitter.test.js'),
+  facets: require('./suites/facets.test.js')
 };
