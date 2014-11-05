15f8eddaa806bf8c81e76927c37ef43184126c9b
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-11-05 14:47:18 +0100

    Added helpers.clone documentation

--- src/domino.helpers.js
+++ src/domino.helpers.js
@@ -2,6 +2,26 @@
 
 var types = require('typology');
 
+/**
+ * This function returns a deep clone of an object. It will copy its scalar
+ * attributes, and recursively clone its complex objects attributes.
+ *
+ * Example:
+ * ********
+ *  > var o1 = {
+ *  >       a: {,
+ *  >         b: 1
+ *  >       }
+ *  >     };
+ *  >     o2 = clone(o1);
+ *  >
+ *  > o1.a.b = 2;
+ *  > console.log(o2.a.b);
+ *  will log 1.
+ *
+ * @param  {object} item The object to clone.
+ * @return {object}      The clone.
+ */
 function clone(item) {
   if (!item)
     return item;
