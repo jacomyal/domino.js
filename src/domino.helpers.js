6ef925201f35224e20826fa4e1f97b0540c3a017
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-11-05 14:54:43 +0100

    Added helpers.extend and helpers.browse
    The static method `helpers.extend(obj1, obj2, ...)` merges different objects into a new one, and returns it.
    It will be used for services, to make it possible to override anything the dev wants when calling the services.
    
    The static method `helpers.browse(obj, transform)` browse deeply (recursively) an object, and returns a new object with scalars transformed through the given function.
    This will allow devs to directly reference properties or facets into services data, to make it dead simple to synchronize server data when front data have been modified.
    
    I also added unit tests for both functions.

--- src/domino.helpers.js
+++ src/domino.helpers.js
@@ -55,6 +55,108 @@ function clone(item) {
   return result;
 }
 
+/**
+ * This function takes any number of objects as arguments, copies from each
+ * of these objects each pair key/value into a new object, and finally
+ * returns this object.
+ *
+ * The arguments are parsed from the last one to the first one, such that
+ * when several objects have keys in common, the "earliest" object wins.
+ *
+ * Example:
+ * ********
+ *  > var o1 = {
+ *  >       a: 1,
+ *  >       b: 2,
+ *  >       c: '3'
+ *  >     },
+ *  >     o2 = {
+ *  >       c: '4',
+ *  >       d: [ 5 ]
+ *  >     };
+ *  > extend(o1, o2);
+ *
+ *  will return:
+ *  > {
+ *  >   a: 1,
+ *  >   b: 2,
+ *  >   c: '3',
+ *  >   d: [ 5 ]
+ *  > };
+ *
+ * @param  {object+} Any number of objects.
+ * @return {object}  The merged object.
+ */
+function extend() {
+  var i,
+      k,
+      res = {},
+      l = arguments.length;
+
+  for (i = l - 1; i >= 0; i--)
+    for (k in arguments[i])
+      res[k] = arguments[i][k];
+
+  return res;
+}
+
+/**
+ * This function will recursively browse an object (or array), and return a deep
+ * clone of this object (or array), but with every scalar objects transformed
+ * with a function given as argument.
+ *
+ * Example:
+ * ********
+ *  > var o = {
+ *  >       a: 'toto',
+ *  >       b: 123,
+ *  >       c: {
+ *  >         d: 'tata',
+ *  >         d: 42
+ *  >       }
+ *  >     };
+ *  > browse(o, function(s) {
+ *  >   return typeof s === 'string' ? s.toUpperCase() : s;
+ *  > });
+ *
+ *  will return:
+ *  > {
+ *  >   a: 'TOTO',
+ *  >   b: 123,
+ *  >   c: {
+ *  >     d: 'TATA',
+ *  >     d: 42
+ *  >   }
+ *  > };
+ *
+ * @param  {object}   item The object to browse.
+ * @param  {function} fn   The function to transform scalars.
+ * @return {object}        The transformed clone.
+ */
+function browse(item, fn) {
+  var i,
+      k,
+      l,
+      res = {};
+
+  if (Array.isArray(item)) {
+    res = [];
+    for (i = 0, l = item.length; i < l; i++)
+      res.push(browse(item[i], fn));
+
+  } else if (item && typeof item === 'object') {
+    res = {};
+    for (k in item)
+      res[k] = browse(item[k], fn);
+
+  } else
+    res = fn(item);
+
+  return res;
+}
+
 module.exports = {
-  clone: clone
+  clone: clone,
+  extend: extend,
+  browse: browse
 };
--- test/suites/helpers.test.js
+++ test/suites/helpers.test.js
@@ -33,14 +33,109 @@ describe('Helpers', function() {
       var e1 = document.createElement('div'),
           e2 = domino.helpers.clone(e1);
 
-      it('should be possible to clone a DOM element.', function() {
+      it('should be possible to clone a DOM element', function() {
         assert.deepEqual(e1, e2);
       });
 
-      it('but the clone should actually be a reference.', function() {
+      it('the clone should actually be a reference', function() {
         e2.a = 2;
         assert.deepEqual(e1, e2);
       });
     }
+  });
+
+  describe('"extend" method', function() {
+    var o1 = {a: 1},
+        o2 = {a: {b: 1}},
+        o3 = {a: 2, b: 1},
+        o4 = {b: 2, c: 1};
+
+    it('should return a clone of the object', function() {
+      var o = domino.helpers.extend(o1);
+      assert.deepEqual(o1, o);
+      o.a = 2;
+      assert(o1.a === 1);
+    });
+
+    it('should not deeply clone a deep object', function() {
+      var o = domino.helpers.extend(o2);
+      assert.deepEqual(o2, o);
+      o.a.b = 2;
+      assert(o2.a.b === 2);
+    });
+
+    it('should merge the latest objects into the first ones, and retrun a new clone', function() {
+      var o = domino.helpers.extend(o1, o3, o4)
+      assert.deepEqual(o, {a: 1, b: 1, c: 1});
+      assert.deepEqual(o1, {a: 1});
+      assert.deepEqual(o3, {a: 2, b: 1});
+      assert.deepEqual(o4, {b: 2, c: 1});
+    });
+  });
+
+  describe('"browse" method', function() {
+    var o = {
+      a: 123,
+      b: 'abc',
+      c: {
+        d: 'def',
+        e: 456
+      },
+      f: [
+        'ghi',
+        789
+      ]
+    };
+
+    it('should return a clone and not alter the original object', function() {
+      domino.helpers.browse(o, function(s) { return false; });
+
+      assert.deepEqual(o, {
+        a: 123,
+        b: 'abc',
+        c: {
+          d: 'def',
+          e: 456
+        },
+        f: [
+          'ghi',
+          789
+        ]
+      });
+    });
+
+    it('should transform the scalars with the given function', function() {
+      var o2 = domino.helpers.browse(
+        o,
+        function(s) {
+          if (typeof s === 'string')
+            return s.toUpperCase();
+          return s;
+        }
+      );
+
+      assert.deepEqual(o2, {
+        a: 123,
+        b: 'ABC',
+        c: {
+          d: 'DEF',
+          e: 456
+        },
+        f: [
+          'GHI',
+          789
+        ]
+      });
+    });
+
+    it('should call the transform function on every scalars, and only scalars', function() {
+      var parsed = [];
+      domino.helpers.browse(
+        [{}, [], new Date(), 1, 'a', false, null, undefined],
+        function(s) { parsed.push(s); }
+      );
+
+      assert.deepEqual(parsed, [1, 'a', false, null, undefined]);
+    });
   });
 });
